f309469d97d70fcc4b6325acab07d7fa2d1e25a6
Brittany DiGenova <Brittany DiGenova>
2016-04-12 21:33:02 -0400

    Brittany DiGenova- Pokemon Classes Submit 02

--- Brittany_DiGenova_lab7_8/main.cpp
+++ Brittany_DiGenova_lab7_8/main.cpp
@@ -13,21 +13,45 @@ using namespace std;
 
 int main() {
 	
-	//Initialize variable
-	Firetype Charizard;
-	Watertype Blastoise;
+	//Initialize firetype pokemon
+	Firetype Charmander("Charmander", 39, 52, 43, 60, 50, 65, 2, 0, 0); //No strengths or weaknesses yet 
+	Firetype Ponyta("Ponyta", 50, 85, 55, 65, 65, 90, 11, 0 , 0);
+	Firetype Magby("Magby", 45, 75, 37, 70, 55, 83, 6, 0, 0);
+	Firetype Flareon("Flareon", 65, 130, 60, 95, 110, 65, 35, 0, 0);
+	Firetype Torchic("Torchic", 45,60, 40, 70, 50, 45, 7, 0, 0);
+	
+	//Initialize watertype pokemon 
+	Watertype Squirtle("Squirtle", 44, 48, 65, 50, 64, 43, 4, 0, 0 );
+	Watertype Horsea("Horsea", 30, 40, 70, 70, 25, 60, 7, 0, 0);
+	Watertype Palpitoad("Tympole", 50, 50, 40, 50, 40, 64, 9, 0, 0);
+	Watertype Mantyke("Mantyke", 45, 20, 50, 60, 120, 50, 7, 0, 0 );
+	Watertype Spheal ("Spheal", 70, 40, 50, 55, 50, 25, 4, 0, 0 );
+	
+	//Initialize Flyingtype Pokemon
+	Flyingtype Caterpie("Caterpie", 45, 30, 35, 20, 20, 45, 2, 0, 0);
+	Flyingtype Zapdos;
+	Flyingtype Pidgey;
+	Flyingtype Zubat;
+	Flyingtype Spearow;
+	Flyingtype Dragonite;
+	
+	//Make kyle, thomas and brittany Pokemon
+	Watertype Kyle("Kyle (AKA Whale)", 52, 60, 55, 61, 64, 80, 5, 0, 0); //No strengths or weaknesses yet
+	Firetype Thomas("Tbone", 55, 58, 64, 61, 70, 74, 5, 0, 0);
+	Flyingtype Brittany("Britters", 60, 54, 61, 63, 62, 40, 5, 0, 0);
+	
 	Potion potion("potion", 20);
 	Potion super_potion ("super_potion", 50);
 	
 	//show Pokemon's hit points before simple healing
-	cout << "Charizard has " << Charizard.get_hit_points() << " hit points." << endl;
+	cout << "Charmander has " << Charmander.get_hit_points() << " hit points." << endl;
 	
 	//heal Pokemon
-	Charizard.heal(potion);
-	Charizard.heal(super_potion);
+	Charmander.heal(potion);
+	Charmander.heal(super_potion);
 	
 	//show Pokemon's hit points after simple healing
-		cout << "Charizard has " << Charizard.get_hit_points() << " hit points." << endl;
+		cout << "Charmander has " << Charmander.get_hit_points() << " hit points." << endl;
 	
 	
 	
--- Brittany_DiGenova_lab7_8/watertype.h
+++ Brittany_DiGenova_lab7_8/watertype.h
@@ -10,7 +10,7 @@ using namespace std;
 class Watertype : public Pokemon {
 	
 	public:
-	Watertype(string name = "none", int hit_points = 0, int attack = 0, int defense = 0, int special_attack = 0, int special_defense = 0, int speed = 0, int level = 2, int strengths = 0, int weaknesses = 0);
+	Watertype( string name = "none", int hit_points = 45, int attack = 45, int defense = 50, int special_attack = 55, int special_defense = 55, int speed = 50, int level = 3, int strengths = 0, int weaknesses = 0 );
 	
 	private:
 	int strengths;
